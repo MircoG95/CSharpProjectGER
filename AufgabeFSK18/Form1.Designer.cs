b783a8c058fd347a7cb1177496e194ac88f958b9
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:44:52 +0100

    Add files via upload

---
+++ AufgabeFSK18/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace AufgabeFSK18
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtp_GebDat = new System.Windows.Forms.DateTimePicker();
+            this.bt_pruefen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // dtp_GebDat
+            // 
+            this.dtp_GebDat.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.dtp_GebDat.Location = new System.Drawing.Point(84, 92);
+            this.dtp_GebDat.Name = "dtp_GebDat";
+            this.dtp_GebDat.Size = new System.Drawing.Size(370, 36);
+            this.dtp_GebDat.TabIndex = 0;
+            // 
+            // bt_pruefen
+            // 
+            this.bt_pruefen.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.bt_pruefen.Location = new System.Drawing.Point(173, 245);
+            this.bt_pruefen.Name = "bt_pruefen";
+            this.bt_pruefen.Size = new System.Drawing.Size(200, 78);
+            this.bt_pruefen.TabIndex = 1;
+            this.bt_pruefen.Text = "Jetzt Prüfen";
+            this.bt_pruefen.UseVisualStyleBackColor = true;
+            this.bt_pruefen.Click += new System.EventHandler(this.bt_pruefen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 381);
+            this.Controls.Add(this.bt_pruefen);
+            this.Controls.Add(this.dtp_GebDat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtp_GebDat;
+        private System.Windows.Forms.Button bt_pruefen;
+    }
+}
+
---
+++ AufgabeFSK18/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AufgabeFSK18
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt_pruefen_Click(object sender, EventArgs e)
+        {
+            int alter = DateTime.Now.Year - dtp_GebDat.Value.Year;
+
+            //Aktuelles: 14.08.2021
+            //GebDatum:  16.08.2000
+
+            if (dtp_GebDat.Value.Month > DateTime.Now.Month || (dtp_GebDat.Value.Month == DateTime.Now.Month
+                && dtp_GebDat.Value.Day > DateTime.Now.Day))
+                alter--;
+
+            if (alter < 18)
+                MessageBox.Show("Zutritt VERWEIGERT");
+            else
+                MessageBox.Show("Zutritt gewährt");
+
+        }
+    }
+}
