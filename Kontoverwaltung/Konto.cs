8363a25985f6ee5f464690b40072913a7444c6b9
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:52:19 +0100

    Add files via upload

---
+++ Kontoverwaltung/Konto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kontoverwaltung
+{
+    class Konto
+    {
+        //Felder
+        private decimal kontostand;
+
+        //Methoden
+        public void GeldEinzahlen(decimal betrag)
+        {
+            kontostand = kontostand + betrag;
+        }
+
+        public void GeldAuszahlen(decimal betrag)
+        {
+            kontostand -= betrag;
+        }
+
+        public decimal KontostandAnzeigen()
+        {
+            if (kontostand < 0)
+                Console.ForegroundColor = ConsoleColor.Red;
+            else
+                Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"Kontostand: {kontostand:f2}");
+            Console.ForegroundColor = ConsoleColor.White;
+            return kontostand;
+        }
+    }
+}
