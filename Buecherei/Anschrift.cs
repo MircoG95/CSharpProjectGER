b26dcdfb2122723b9fb32d2b1ce8dea7852a1507
Mirco <91058439+MircoG95@users.noreply.github.com>
2023-01-27 17:45:15 +0100

    Add files via upload

---
+++ Buecherei/Anschrift.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buecherei
+{
+    class Anschrift
+    {
+        //Felder
+        private string strasse;
+        private string hausNr;
+        private string plz;
+        private string ort;
+    }
+}
---
+++ Buecherei/Verlag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buecherei
+{
+    class Verlag
+    {
+        //Felder
+        private string firmenName;
+        private Anschrift anschrift;
+
+    }
+}
